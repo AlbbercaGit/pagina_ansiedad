2ad92a8f4d9d4ea7ee58ecde899ed89307dbe608
unknown <alejandroalbbercabarcelona@gmail.com>
2024-12-11 10:32:46 +0100

    nueva subida nombre repo

--- ansiedad-caramelizada/next.config.ts
+++ ansiedad-caramelizada/next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from 'next'
 
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'pagina_ansiedad' // Nombre exacto del repositorio
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true
   },
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: isProd ? `/${repoName}` : '',
+  trailingSlash: true,
 }
 
 export default nextConfig
