55e31810b8884128aa5dff771047728566548339
unknown <alejandroalbbercabarcelona@gmail.com>
2024-12-11 13:51:45 +0100

    Que mas 3

--- ansiedad-caramelizada/src/app/layout.tsx
+++ ansiedad-caramelizada/src/app/layout.tsx
@@ -11,6 +11,20 @@ const roboto = Roboto({
 export const metadata: Metadata = {
   title: 'Ansiedad Caramelizada',
   description: 'Sitio oficial de Ansiedad Caramelizada',
+  openGraph: {
+    title: 'Ansiedad Caramelizada',
+    description: 'Sitio oficial de Ansiedad Caramelizada',
+    images: [
+      {
+        url: 'https://raw.githubusercontent.com/AlbbercaGit/videos/main/ansiedadcaramelizada-ep-cover-redes.jpg',
+        width: 1200,
+        height: 1200,
+        alt: 'Ansiedad Caramelizada',
+      },
+    ],
+    locale: 'es_ES',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
