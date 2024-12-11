d2026ad517d1625f2c7cc1969e74111df0ff6351
unknown <alejandroalbbercabarcelona@gmail.com>
2024-12-11 10:22:45 +0100

    nueva subida json y ts

--- ansiedad-caramelizada/next.config.ts
+++ ansiedad-caramelizada/next.config.ts
@@ -3,10 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
-  assetPrefix: '/ansiedad-caramelizada/',
-  basePath: '/ansiedad-caramelizada',
 }
 
 export default nextConfig
